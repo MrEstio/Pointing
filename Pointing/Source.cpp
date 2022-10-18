ce2419de25f953752dd077140487f70411754af4
Deku <Deku@PREDATOR>
2022-10-18 22:22:50 +0300

    Добавьте файлы проекта.

---
+++ Pointing/Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int* pAPointer; // объявляем указатель
+	int* pScore = 0; // объявляем и инициализируем указатель
+	int score = 1000;
+	pScore = &score; // присваиваем указателю pScore адрес переменной score
+	cout << "Assigning &score to pScore\n";
+	cout << "&score is: " << &score << "\n"; // адрес переменной score
+	cout << "&pScore is: " << &pScore << "\n"; // адрес, сохраненный в указателе
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n"; // значение, на которое направлен указатель
+	cout << "Adding 500 to score\n";
+	score += 500;
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n"; // значение, на которое направлен указатель
+	cout << "Adding 500 to *pScore\n";
+	cout << "Assing &newScore to pScore";
+	int newScore = 5000;
+	pScore = &newScore;
+	cout << "&newScore is: " << &newScore << "\n";
+	cout << "pScore is: " << pScore << "\n\n";
+	cout << "newScore is: " << newScore << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+	cout << "Assing &str to pStr\n";
+	string str = "score";
+	string* pStr = &str; // указатель на строковый объект
+	cout << "str is: " << str << "\n";
+	cout << "*pStr is: " << *pStr << "\n";
+	cout << "(*pstr).size() is: " << (*pStr).size() << "\n";
+	cout << "pStr->size is: " << pStr->size() << "\n";
+	return 0;
+
+
+
+
+
+
+
+
+
+
+
+}
